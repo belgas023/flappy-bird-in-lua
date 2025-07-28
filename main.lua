1034c0c01d0c567c4deccb839ab8b522a487fa4a
bellagio <stephanehugon@Bellagio.local>
2025-07-28 16:33:14 +0200

    basic structure v2

---
+++ main.lua
@@ -0,0 +1,18 @@
+local bird = {
+    x = 20,
+    y = 20,
+    sprite = nil,
+}
+
+
+function love.load()
+    bird.sprite = love.graphics.newImage('assets/bird.png')
+end
+
+function love.update(dt)
+
+end
+
+function love.draw()
+
+end
