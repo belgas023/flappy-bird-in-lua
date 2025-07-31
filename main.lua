0bc4cee160e8e02bc78a07678d8176ce8a7c8f10
bellagio <stephanehugon@Bellagio.local>
2025-08-01 00:47:29 +0200

    obstacles fix

--- main.lua
+++ main.lua
@@ -12,17 +12,51 @@ function love.load()
 end
 
 -- constantes
-local SPEED = 10
 local GRAVITY = 700
 local UP = 700
 local vmax = 400
 local vmin = -300
 local gravier = 1
 local timer = 0
+local pipes = {}
 
 function love.keypressed(key)
     if key == 'up' then
         bird.vy = bird.vy - UP
+    end
+end
+
+local function pipeSpawn()
+    Pipe = {
+        x = love.graphics.getWidth(),
+        y = 0,
+        width = 30,
+        height = love.math.random(20, 150),
+    }
+    table.insert(pipes, Pipe)
+end
+
+local function pipeMovement(dt)
+    for i, pipe in ipairs(pipes) do
+        pipe.x = pipe.x - 100 * dt
+    end
+end
+
+local function isColliding(thing, object)
+    local ax1 = thing.x
+    local ax2 = thing.x + thing.sprite:getWidth()
+    local ay1 = thing.y
+    local ay2 = thing.y + thing.sprite:getHeight()
+
+    local bx1 = object.x
+    local bx2 = object.x + object.width
+    local by1 = object.y
+    local by2 = object.y + object.height
+
+    if ax1 < bx2 and ax2 > bx1 and ay1 < by2 and ay2 > by1 then
+        return true
+    else
+        return false
     end
 end
 
@@ -36,13 +70,19 @@ function love.update(dt)
         bird.vy = vmin
     end
 
-
-    Obstacle = false
     timer = timer + 1
     if timer == 50 then
         timer = 0
         --obstacle
-        Obstacle = true
+        pipeSpawn()
+    end
+
+    pipeMovement(dt)
+
+    for i, pipe in ipairs(pipes) do
+        if isColliding(bird, pipe) then
+            GameOver = true
+        end
     end
 end
 
@@ -55,4 +95,12 @@ function love.draw()
     love.graphics.draw(bird.sprite, bird.x, bird.y)
 
     love.graphics.print(bird.vy, 10, 10)
+
+    for i, pipe in ipairs(pipes) do
+        love.graphics.rectangle('fill', pipe.x, pipe.y, pipe.width, pipe.height)
+    end
+
+    if GameOver then
+        love.graphics.print('game over', 50, 50)
+    end
 end
