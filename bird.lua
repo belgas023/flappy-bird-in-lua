6c44ba752eb73e6a41ef9dd6507587fed280186a
bellagio <stephanehugon@Bellagio.local>
2025-08-01 22:48:25 +0200

    modularization

---
+++ bird.lua
@@ -0,0 +1,69 @@
+local pipes = require('pipes')
+
+local bird = { --table bird
+    x = 145,
+    vx = 0,
+    y = 192,
+    vy = 0,
+    sprite = nil,
+    orientation = 0.5
+}
+
+local GRAVITY = 700
+local gravier = 1
+local vmax = 400
+local vmin = -300
+local UP = 700
+
+local function isColliding(thing, object)
+    local ax1 = thing.x
+    local ax2 = thing.x + thing.sprite:getWidth()
+    local ay1 = thing.y
+    local ay2 = thing.y + thing.sprite:getHeight()
+
+    local bx1 = object.x
+    local bx2 = object.x + object.width
+    local by1 = object.y
+    local by2 = object.y + object.height
+
+    if ax1 < bx2 and ax2 > bx1 and ay1 < by2 and ay2 > by1 then
+        return true
+    else
+        return false
+    end
+end
+
+function bird.load()
+    bird.sprite = love.graphics.newImage('assets/bird.png')
+end
+
+function bird.update(dt)
+    bird.vy = bird.vy + GRAVITY * dt
+    bird.y = bird.y + bird.vy * dt * gravier
+
+    if bird.vy > vmax then
+        bird.vy = vmax
+    elseif bird.vy < vmin then
+        bird.vy = vmin
+    end
+
+    for i, pipe in ipairs(pipes) do
+        if isColliding(bird, pipe) then
+            love.graphics.print('game over', 145, 300)
+        end
+    end
+end
+
+function bird.draw()
+    love.graphics.draw(bird.sprite, bird.x, bird.y)
+
+    love.graphics.print(bird.vy, 10, 10)
+end
+
+function love.keypressed(key)
+    if key == 'up' then
+        bird.vy = -UP
+    end
+end
+
+return bird
---
+++ pipes.lua
@@ -0,0 +1,41 @@
+local pipes = {}
+local timer = 0
+local DelayPipe = 100
+
+local function pipeSpawn()
+    Pipe = {
+        x = love.graphics.getWidth(),
+        y = 0,
+        width = 50,
+        height = love.math.random(20, love.graphics.getHeight() / 3),
+    }
+    table.insert(pipes, Pipe)
+end
+
+local function pipeMovement(dt)
+    for i, pipe in ipairs(pipes) do
+        pipe.x = pipe.x - 100 * dt
+    end
+end
+
+function pipes.load()
+
+end
+
+function pipes.update(dt)
+    timer = timer + 1
+    if timer == DelayPipe then
+        timer = 0
+        pipeSpawn()
+    end
+    pipeMovement(dt)
+    print(pipes)
+end
+
+function pipes.draw()
+    for i, pipe in ipairs(pipes) do
+        love.graphics.rectangle('fill', pipe.x, pipe.y, pipe.width, pipe.height)
+    end
+end
+
+return pipes
